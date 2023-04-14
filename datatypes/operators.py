45cbd06cf0cc3c45c57648bf6e50dacc1bd7e477
Pasupathi M <pasupathip396@gmail.com>
2023-04-14 14:05:58 +0530

    Pyhton data types starter

---
+++ datatypes/operators.py
@@ -0,0 +1,16 @@
+# PEMDASLR
+
+# P - Paranthesis
+# E - Exponen
+# M - Muliplication
+# D - Division
+# A - Addition
+# S - Subtract
+
+# + - Additon
+# - - Subtract
+# * - Multiply
+# / - Division
+# // - Absoulte division
+# ** - Exponent
+
