11f7ec7311d12e7705cf20850cfabd6a3aec1d1d
Pasupathi-M <pasupathi.muthukumar@kprinfo.com>
2023-09-01 19:55:49 +0530

    VARIABLE access issue

--- test.py
+++ test.py
@@ -1,1 +1,10 @@
 print(len([2343, True,  457]))
+
+
+# variable access before declare
+try:
+    print(hello)
+
+    hello = 'Hi'
+except NameError as NE:
+    print('NameError: Variable declaration issue')
